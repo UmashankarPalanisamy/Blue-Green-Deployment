9416d61eadb3e78b876c35aa8f72084eed0b0c56
UmashankarPalanisamy <umashankarp85@gmail.com>
2025-01-04 13:32:45 +0530

    Update output.tf

--- Cluster/output.tf
+++ Cluster/output.tf
@@ -1,16 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.bluegreenums.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.bluegreenums.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.bluegreenums_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.bluegreenums_subnet[*].id
 }
-
